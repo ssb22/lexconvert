447f8b05548e45e4fc8d5a20f92b60995ac5894a
Silas S. Brown <ssb22@cam.ac.uk>
2015-06-29 13:51:43 +0000

    Update lexconvert
    git-svn-id: http://svn.code.sf.net/p/e-guidedog/code/ssb22/lexconvert@2234 29193198-4895-4776-b068-10539e920549

--- lexconvert.py
+++ lexconvert.py
@@ -2095,7 +2095,7 @@ You need to specify the location of the OALD file in <location>,
 e.g. for Debian festlex-oald package: python lexconvert.py --festival-dictionary-to-espeak /usr/share/festival/dicts/oald/all.scm
 or if you can't install the Debian package, try downloading http://ftp.debian.org/debian/pool/non-free/f/festlex-oald/festlex-oald_1.4.0.orig.tar.gz, unpack it into /tmp, and do: python lexconvert.py --festival-dictionary-to-espeak /tmp/festival/lib/dicts/oald/oald-0.4.out
 In all cases you need to cd to the eSpeak source directory before running this.  en_extra will be overwritten.  Converter will also read your ~/.festivalrc if it exists.  (You can later incrementally update from ~/.festivalrc using the --convert option; the entries from the system dictionary will not be overwritten in this case.)  Specify --without-check to bypass checking the existing eSpeak pronunciation for OALD entries (much faster, but makes a larger file and in some cases compromises the pronunciation quality)."""
-   try: festival_location=sys.argv[i]
+   try: festival_location=sys.argv[i+1]
    except IndexError: return "Error: --festival-dictionary-to-espeak must be followed by the location of the festival OALD file (see help text)"
    try: open(festival_location)
    except: return "Error: The specified OALD location '"+festival_location+"' could not be opened"
