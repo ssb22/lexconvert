893c23ed680b7760d4488f9d9e535a4f6e0c1bec
Silas S. Brown <ssb22@cam.ac.uk>
2018-02-19 07:57:47 -0600

    Update lexconvert

--- lexconvert.py
+++ lexconvert.py
@@ -3061,7 +3061,7 @@ def main():
               sys.stderr.write(msg+"\n") ; return 1
            else: return 0
     html = ('--htmlhelp' in sys.argv) # (undocumented option used for my website, don't rely on it staying)
-    def htmlify(h): return h.replace('&','&amp;').replace('<','&lt;').replace('>','&gt;').replace('\n','<br>').replace('--','<tt>--</tt>') # (the last bit is so typography.js doesn't try to rewrite options stuff to en-dash)
+    def htmlify(h): return h.replace('&','&amp;').replace('<','&lt;').replace('>','&gt;').replace('\n','<br>').replace('--','<kbd>--</kbd>') # (the last bit is so typography.js doesn't try to rewrite options stuff to en-dash)
     if not html: htmlify = lambda x:x
     print htmlify(__doc__)
     if html: missALine = "<p>"
